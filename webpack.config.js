cda43a96c5a43d4d8afc7e2f4422466d9cf215d9
rockmania-jp <gym@rockmania.org>
2022-12-22 18:34:57 +0900

    add main image

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     ],
     entry: [
 	path.join(__dirname, "src/js/scale.fix.js"),
-	//path.join(__dirname, "src/js/slider.js"),
+	path.join(__dirname, "src/js/slider.js"),
 	path.join(__dirname, "src/js/insta.js"),
 	path.join(__dirname, "src/js/facebook.js")
     ],
