0ce54664e3bd72f0302ccc83cdce42babf999bbd
rockmania-jp <gym@rockmania.org>
2023-01-05 12:23:49 +0900

    skip reel

--- src/js/insta.js
+++ src/js/insta.js
@@ -13,12 +13,15 @@
 	    let i = 0;
 	    let html = '';
 	    for (let item of json.media.data) {
-		if (i < 3) {
-		    let url     = item.media_url;
-		    let href    = item.permalink;
-		    let caption = item.caption;
-		    html += `<li><a href="${href}" target="_blank"><img src="${url}" alt="${caption}"><p>${caption}</p></a></li>`;
-		    i++;
+		// リールは除く
+		if (item.media_url) {
+		    if (i < 3) {
+			let url     = item.media_url;
+			let href    = item.permalink;
+			let caption = item.caption;
+			html += `<li><a href="${href}" target="_blank"><img src="${url}" alt="${caption}"><p>${caption}</p></a></li>`;
+			i++;
+		    }
 		}
 	    }
 	    document.querySelector('.insta-list').innerHTML = html;
